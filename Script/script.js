ba8abc7af34a5bbc56f5e0befa8960a1f74764c8
hridayshadhukha <hridayshadhukha@gmail.com>
2026-03-01 19:23:44 +0600

    Load all the Catagory Button From API

---
+++ Script/script.js
@@ -0,0 +1,23 @@
+
+
+function loadCategories(){
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+  .then((res) => res.json())
+  .then((data) => displayCategories(data.categories))
+}
+
+function displayCategories(categories){
+const categoryContainer = document.getElementById("category-container");
+
+for(let cat of categories){
+ const categoryDiv = document.createElement("div");
+ categoryDiv.innerHTML = `
+ <button class="btn btn-sm text-lg">${cat.category}</button>
+ `
+
+ categoryContainer.append(categoryDiv)
+}
+
+}
+
+loadCategories()
