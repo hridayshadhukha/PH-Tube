afe36969ae79edef57776573b14e13a051ee14b4
hridayshadhukha <hridayshadhukha@gmail.com>
2026-03-02 18:56:34 +0600

    add Dynamic Videos title with api

--- Script/script.js
+++ Script/script.js
@@ -1,23 +1,44 @@
-
-
-function loadCategories(){
+function loadCategories() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-  .then((res) => res.json())
-  .then((data) => displayCategories(data.categories))
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories));
 }
 
-function displayCategories(categories){
-const categoryContainer = document.getElementById("category-container");
-
-for(let cat of categories){
- const categoryDiv = document.createElement("div");
- categoryDiv.innerHTML = `
- <button class="btn btn-sm text-lg">${cat.category}</button>
- `
-
- categoryContainer.append(categoryDiv)
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos));
 }
 
+function displayCategories(categories) {
+  const categoryContainer = document.getElementById("category-container");
+
+  for (let cat of categories) {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+ <button class="btn btn-sm text-lg hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+ `;
+
+    categoryContainer.append(categoryDiv);
+  }
 }
 
-loadCategories()
+const displayVideos = (videos) => {
+  const videosContainer = document.getElementById("videos-container");
+
+  videos.forEach((video) => {
+    console.log(video);
+
+    const videoCard = document.createElement("div");
+    videoCard.innerHTML = `
+    
+    <h2 class="text-xl">${video.title}</h2>
+
+    `
+
+    videosContainer.append(videoCard)
+  });
+};
+
+loadCategories();
+loadVideos();
