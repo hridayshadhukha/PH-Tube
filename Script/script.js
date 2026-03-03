d851d7e33d8bdb843db5545f6fe6bf14a903867b
hridayshadhukha <hridayshadhukha@gmail.com>
2026-03-03 19:25:44 +0600

    succesfully add comedy and funny videos dynamically

--- Script/script.js
+++ Script/script.js
@@ -10,13 +10,28 @@ function loadVideos() {
     .then((data) => displayVideos(data.videos));
 }
 
+const loadCategoryVideos = (id) => {
+  
+  const url= `
+  https://openapi.programming-hero.com/api/phero-tube/category/${id}
+  `
+
+ 
+
+
+fetch(url)
+.then((res) => res.json())
+.then((data) => displayVideos(data.category))
+
+};
+
 function displayCategories(categories) {
   const categoryContainer = document.getElementById("category-container");
 
   for (let cat of categories) {
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
- <button class="btn btn-sm text-lg hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+ <button onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm text-lg hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
  `;
 
     categoryContainer.append(categoryDiv);
@@ -61,11 +76,10 @@ const displayVideos = (videos) => {
           </div>
         </div>
 
-    `
+    `;
 
-    videosContainer.append(videoCard)
+    videosContainer.append(videoCard);
   });
 };
 
 loadCategories();
-loadVideos();
